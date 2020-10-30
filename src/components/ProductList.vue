b241b5707e2926d31d078ae15a9e75a66ba786c1
VABarsukov <VBarsukov@sportmaster.ru>
2020-10-30 21:09:43 +0300

    add components ProductList и ProductItems

---
+++ src/components/ProductList.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul class="catalog__list"> 
+      <li class="catalog__item" v-for="product in products" :key="product.id">        
+          <ProductItem :product="product"/>        
+      </li>
+    </ul>
+</template>
+
+<script>
+import ProductItem from './ProductItem'
+
+export default {
+    props: ['products'],
+    components: {
+        ProductItem
+    }
+}
+</script>
