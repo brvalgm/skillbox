55c6d83f53cdf119a7a1c6358a21f220720fd821
VABarsukov <VBarsukov@sportmaster.ru>
2020-11-04 12:13:38 +0300

    Дабвлена фильтрация по цене и категориям

--- src/modules/data/products.js
+++ src/modules/data/products.js
@@ -1,102 +1,119 @@
 export default [
     {
-        id:    1,     
+        id:    1,
+        categoryid: 4,     
         title: 'Радионяня Motorola MBP16',
         price: 3690,
         image: 'img/radio.jpg'
     },
     {
         id:    2,
+        categoryid: 2,
         title: 'Смартфон Samsung Galaxy A01 16Gb Черный',
         price: 6990,
         image: 'img/smartfon_samsung_galaxy_a01_16gb_black_1254032_1.jpg'
     },
     {
-        id:    3, 
+        id:    3,
+        categoryid: 2, 
         title: 'Смартфон Samsung Galaxy M11 32Gb Черный',
         price: 10990,
         image: 'img/samsung_galaxy_m11_32gb_chernyy_1393418_1.jpg'
     },
     {
         id:    4,
+        categoryid: 6,
         title: 'Холодильник Bosch KGV36NL1AR',
         price: 26990 ,
         image: 'img/bosch_kgv36nl1ar_1.jpg'
     },
     {
         id:    5,
+        categoryid: 3,
         title: 'Гироскутер',
         price: 12490,
         image: 'img/wheels.jpg'
     },
     {
-        id:    6, 
+        id:    6,
+        categoryid: 6, 
         title: 'Стиральная машина Electrolux PerfectCare 600 EW6S4R06W',
         price: 23990,
         image: 'img/stiralnaya_mashina_s_frontalnoy_zagruzkoy_electrolux_ew6s4r06w_907322_1.jpg'
     },
     {
-        id:    7, 
+        id:    7,
+        categoryid: 3, 
         title: 'Детский велосипед',
         price: 31890,
         image: 'img/bicycle.jpg'
     },
     {
         id:    8,
+        categoryid: 4,
         title: 'HUAWEI MatePad T8 32Gb LTE Deep Blue',
         price: 9990,
         image: 'img/planshet_huawei_mediapad_t8_32gb_lte_deep_blue_1447337_1.jpg'
     },
     {
         id:    9,
+        categoryid: 5,
         title: 'Наушникик Sony T450',
         price: 2500,
         image: 'img/headphones.jpg'
     },
     {
         id:    10,
+        categoryid: 4,
         title: 'Samsung Galaxy Tab A 8.0 (2019) SM-T295 32Gb LTE (черный)',
         price: 12990,
         image: 'img/planshetnyy_kompyuter_samsung_galaxy_tab_a_8.0_2019_sm_t295_32gb_lte_chernyy__1143944_1.jpg'
     },
     {
         id:    11,
+        categoryid: 4,
         title: 'Смарт-часы Xiaomi Amazfit GTS, Obsidian Black',
         price: 9990,
         image: 'img/smart_chasy_xiaomi_amazfit_gts_obsidian_black_1220077_1.jpg'
     },
     {
         id:    12,
+        categoryid: 2,
         title: 'Мобильный телефон Nokia 105 (TA-1174) Black',
         price: 1290,
         image: 'img/1300360_1.png'
     },
     {
         id:    13,
+        categoryid: 4,
         title: 'Цифровой MP3-плеер Ritmix RF-4950 4Gb black',
         price: 1550,
         image: 'img/ritmix_rf_4950_4gb_black_1.jpg'
     },
     {
         id:    14,
+        categoryid: 3,
         title: 'Электрический самокат',
         price: 5800,
         image: 'img/scooter.jpg'
     },
     {
         id:    15,
+        categoryid: 1,
         title: 'Зубная щетка',
         price: 2300,
         image: 'img/toothbrush.jpg'
     } ,
     {
         id:    16,
+        categoryid: 3,
         title: 'Доска для катания',
         price: 2300,
         image: 'img/board.jpg'
     } ,
     {
         id:    17,
+        categoryid: 5,
         title: 'Наушникик Airpods',
         price: 2300,
         image: 'img/airpods.jpg'
