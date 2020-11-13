12422496101ddf58f261c6c656cc183f7be35039
VABarsukov <VBarsukov@sportmaster.ru>
2020-11-13 15:28:29 +0300

    Изучил работу с vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,11 @@
 <template>
-  <component :is="currentPageCompanent" :page-params="currentPageParams" />
+  <router-view />
 </template>
 
 <script>
-import MainPage from './pages/MainPage';
-import ProductPage from './pages/ProductPage';
-import NotFoundPage from './pages/NotFoundPage';
-import eventBus from '@/eventBus';
-
-const routes = {
-  main: "MainPage",
-  product: "ProductPage"
-}
-
 export default {
   name: 'App',
-  data() {
-    return {
-      currentPage: 'main',
-      currentPageParams: {}
-    }
-  },
-  components: { MainPage, ProductPage, NotFoundPage },
-  computed: {
-    currentPageCompanent() {
-      return routes[this.currentPage] || 'NotFoundPage';
-    }
-  
-  },
-  methods: {
-    gotoPage(pageName, pageParams) {
-      this.currentPage = pageName;
-      this.currentPageParams = pageParams || {};
-    }
-  },
-  created() {
-    eventBus.$on('goto-page', (pageName, pageParam) => this.gotoPage(pageName, pageParam));
-  }
+
 }
 </script>
 
