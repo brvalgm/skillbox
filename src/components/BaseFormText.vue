a6abb291da923f35e52458d4c8fa1829bf457023
VABarsukov <VBarsukov@sportmaster.ru>
2020-11-15 16:45:26 +0300

    Форма оформления заказов. Слоты и примеси.

---
+++ src/components/BaseFormText.vue
@@ -0,0 +1,18 @@
+<template>
+  <BaseFormField :title="title" :error="error"> 
+    <input class="form__input" :type="type" v-model="dataValue" :placeholder="placeholder">
+  </BaseFormField>  
+</template>
+
+<script>
+import formFieldMixin from '@/mixins/FormFieldMixin';
+
+export default {
+    props: {
+        type: {
+            default: 'text'
+        }
+    },
+    mixins: [ formFieldMixin ]
+}
+</script>
---
+++ src/components/BaseFormTextArea.vue
@@ -0,0 +1,13 @@
+<template>
+  <BaseFormField :title="title" :error="error"> 
+    <textarea class="form__input form__input--area" v-model="dataValue" :placeholder="placeholder"></textarea>
+  </BaseFormField>  
+</template>
+
+<script>
+import formFieldMixin from '@/mixins/FormFieldMixin';
+
+export default {
+    mixins: [ formFieldMixin ]
+}
+</script>
